e1f839a6a2b7e88011260d17c103aa096d14ceb7
pgalussoOactive <pgalusso@overactive.com>
2020-01-22 05:12:57 -0300

    Merges client to a single client

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -43,5 +44,34 @@ class BreweryClientTest {
     @Test
     void deleteBeer() {
         client.deleteBeer(UUID.randomUUID());
+    }
+
+    @Test
+    void getCustomerByUUID() {
+        CustomerDto customerDto = client.getCustomerByUUID(UUID.randomUUID());
+
+        assertNotNull(customerDto);
+    }
+
+    @Test
+    void saveNewCustomer() {
+        CustomerDto newCustomer = CustomerDto.builder().name("Pepito Gomez").build();
+
+        URI uri = client.saveNewCustomer(newCustomer);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void updateCustomer() {
+        CustomerDto customer = CustomerDto.builder().name("Pepito Gomez").build();
+        client.updateCustomer(UUID.randomUUID(), customer);
+    }
+
+    @Test
+    void deleteCustomer() {
+        client.deleteCustomer(UUID.randomUUID());
     }
 }
