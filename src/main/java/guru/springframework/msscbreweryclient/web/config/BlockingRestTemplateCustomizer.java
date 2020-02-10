304cfd25cc4d9ed2d6a88865e7a44584ec7ab13b
pgalussoOactive <pgalusso@overactive.com>
2020-02-10 01:51:40 -0300

    Adds personal externalization

--- src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -5,6 +5,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -13,15 +14,25 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+    @Value("${pooling.maxTotal}")
+    private int poolingMaxTotal;
+    @Value("${pooling.maxPerRoute}")
+    private int poolingMaxPerRoute;
+
+    @Value("${request.connectionTimeout}")
+    private int requestConnectionTimeout;
+    @Value("${request.socketTimeout}")
+    private int requestSocketTimeout;
+
     public ClientHttpRequestFactory clientHttpRequestFactory() {
         PoolingHttpClientConnectionManager connectionManager =new PoolingHttpClientConnectionManager();
-        connectionManager.setMaxTotal(100);
-        connectionManager.setDefaultMaxPerRoute(20);
+        connectionManager.setMaxTotal(poolingMaxTotal);
+        connectionManager.setDefaultMaxPerRoute(poolingMaxPerRoute);
 
         RequestConfig requestConfig = RequestConfig
                 .custom()
-                .setConnectionRequestTimeout(3000)
-                .setSocketTimeout(3000)
+                .setConnectionRequestTimeout(requestConnectionTimeout)
+                .setSocketTimeout(requestSocketTimeout)
                 .build();
 
         CloseableHttpClient httpClient = HttpClients
